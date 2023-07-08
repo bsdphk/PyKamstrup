ba1d870464d8bb66a22bf90003d9e1702c00145d
oldchaprk <Raymond_Kloprogge@hotmail.com>
2023-07-08 19:13:02 +0200

    Add various power meters, add icecream, select different port

--- kamstrup.py
+++ kamstrup.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 #
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
@@ -10,90 +10,169 @@
 
 from __future__ import print_function
 
-# You need pySerial 
+# You need pySerial
 import serial
+from icecream import ic
 
 import math
+import sys
 
 #######################################################################
 # These are the variables I have managed to identify
 # Submissions welcome.
 
 kamstrup_382_var = {
+  0x0001: "Energy in",
+  0x0002: "Energy out",
 
-	0x0001: "Energy in",
-	0x0002: "Energy out",
+  0x000d: "Energy in hi-res",
+  0x000e: "Energy out hi-res",
 
-	0x000d: "Energy in hi-res",
-	0x000e: "Energy out hi-res",
+  0x041e: "Voltage p1",
+  0x041f: "Voltage p2",
+  0x0420: "Voltage p3",
 
-	0x041e: "Voltage p1",
-	0x041f: "Voltage p2",
-	0x0420: "Voltage p3",
+  0x0434: "Current p1",
+  0x0435: "Current p2",
+  0x0436: "Current p3",
 
-	0x0434: "Current p1",
-	0x0435: "Current p2",
-	0x0436: "Current p3",
+  0x0056: "Current flow temperature",
+  0x0057: "Current return flow temperature",
+  0x0058: "Current temperature T3",
+  0x007A: "Current temperature T4",
+  0x0059: "Current temperature difference",
+  0x005B: "Pressure in flow",
+  0x005C: "Pressure in return flow",
+  0x004A: "Current flow in flow",
+  0x004B: "Current flow in return flow",
+  0x03ff: "Power In",
+  0x0438: "Power p1 In",
+  0x0439: "Power p2 In",
+  0x043a: "Power p3 In",
 
-	0x0438: "Power p1",
-	0x0439: "Power p2",
-	0x043a: "Power p3",
+  0x0400: "Power In",
+  0x0540: "Power p1 Out",
+  0x0541: "Power p2 Out",
+  0x0542: "Power p3 Out",
+}
 
-	0x0056: "Current flow temperature",
-	0x0057: "Current return flow temperature",
-	0x0058: "Current temperature T3",
-	0x007A: "Current temperature T4",
-	0x0059: "Current temperature difference",
-	0x005B: "Pressure in flow",
-	0x005C: "Pressure in return flow",
-	0x004A: "Current flow in flow",
-	0x004B: "Current flow in return flow"
-	0x03ff: "Power In",
-	0x0438: "Power p1 In",
-	0x0439: "Power p2 In",
-	0x043a: "Power p3 In",
+kamstrup_681_var = {
+  1:	"Date",
+  60:	"Heat",
+  61:	"x",
+  62:	"x",
+  63:	"x",
+  95:	"x",
+  96:	"x",
+  97:	"x",
+}
 
-	0x0400: "Power In",
-	0x0540: "Power p1 Out",
-	0x0541: "Power p2 Out",
-	0x0542: "Power p3 Out",
+kamstrup_MC601_var = {
+  0x003C: "Energy register 1: Heat energy",
+  0x0044: "Volume register V1",
+  0x0058: "Current temperature T3",
+  0x03EC: "Operation hours counter",
+}
+
+kamstrup_multical402_var = ({'baudrate':1200},{
+	0x003C: "Energy",
+	0x0044: "Volume",
+	0x004A: "Flow",#"Current flow in flow",
+	0x004B: "ReturnFlow",#"Current flow in return flow"
+	0x0050: "Power",
+	0x0056: "FlowTemp",# "Current flow temperature",
+	0x0057: "RetFlowTemp",#"Current return flow temperature",
+	0x0058: "T3Temp",#"Current temperature T3",
+	0x0059: "DiffTemp",#"Current temperature difference",
+	0x005B: "FlowPressure",#"Pressure in flow",
+	0x005C: "ReturnFlowPressure",#"Pressure in return flow",
+	0x007A: "T4Temp",#"Current temperature T4",
+})
+
+kamstrup_MC21_var = {
+	0x44: 'Volume register V1', # (m3)
+	0x4A: 'Current flow', # (l/h)
+	0xEF: 'Volume', #  (l)
+}
+
+kamstrup_MC403_var = {
+	0x3C: 'Energy register 1: Heat energy', # (MWh)
+	0x50: 'Current Power', #  (kW)
+	0x56: 'Current flow temperature', # (C)
+	0x57: 'Current return flow temperature', # (C)
+	0x59: 'Current temperature difference', # (K)
+	0x4A: 'Current flow', # (l/h)
+	0x44: 'Volume register V1', # (m3)
 }
 
 kamstrup_681_var = {
 	1:	"Date",
 	60:	"Heat",
-	61:	"x",
-	62:	"x",
-	63:	"x",
-	95:	"x",
-	96:	"x",
-	97:	"x",
 }
 
 kamstrup_MC601_var = {
-	0x003C: "Energy register 1: Heat energy",
-	0x0044: "Volume register V1",
-	0x0058: "Current temperature T3",
-	0x03EC: "Operation hours counter",
+        0x003C: "Energy register 1: Heat energy",
+        0x0044: "Volume register V1",
+        0x0058: "Current temperature T3",
+        0x03EC: "Operation hours counter",
 }
+
+kamstrup_162J_var = {
+
+    0x0001: "Energy-in-low-res",
+    0x0002: "Energy-out-low-res",
+
+    0x000d: "Ap",
+    0x000e: "Am",
+
+    0x041e: "U1",
+
+    0x0434: "I1",
+
+    0x0438: "P1",
+    0x03e9: 'Meter-serialnumber',
+}
+
+kamstrup_362J_var = {
+
+    0x0001: "Energy-in-low-res",
+    0x0002: "Energy-out-low-res",
+
+    0x000d: "Ap",
+    0x000e: "Am",
+
+    0x041e: "U1",
+    0x041f: "U2",
+    0x0420: "U3",
+
+    0x0434: "I1",
+    0x0435: "I2",
+    0x0436: "I3",
+
+    0x0438: "P1",
+    0x0439: "P2",
+    0x043a: "P3",
+    0x03e9: 'Meter-serialnumber',
+}
+
 
 
 #######################################################################
 # Units, provided by Erik Jensen
 
 units = {
-	0: '', 1: 'Wh', 2: 'kWh', 3: 'MWh', 4: 'GWh', 5: 'j', 6: 'kj', 7: 'Mj',
-	8: 'Gj', 9: 'Cal', 10: 'kCal', 11: 'Mcal', 12: 'Gcal', 13: 'varh',
-	14: 'kvarh', 15: 'Mvarh', 16: 'Gvarh', 17: 'VAh', 18: 'kVAh',
-	19: 'MVAh', 20: 'GVAh', 21: 'kW', 22: 'kW', 23: 'MW', 24: 'GW',
-	25: 'kvar', 26: 'kvar', 27: 'Mvar', 28: 'Gvar', 29: 'VA', 30: 'kVA',
-	31: 'MVA', 32: 'GVA', 33: 'V', 34: 'A', 35: 'kV',36: 'kA', 37: 'C',
-	38: 'K', 39: 'l', 40: 'm3', 41: 'l/h', 42: 'm3/h', 43: 'm3xC',
-	44: 'ton', 45: 'ton/h', 46: 'h', 47: 'hh:mm:ss', 48: 'yy:mm:dd',
-	49: 'yyyy:mm:dd', 50: 'mm:dd', 51: '', 52: 'bar', 53: 'RTC',
-	54: 'ASCII', 55: 'm3 x 10', 56: 'ton x 10', 57: 'GJ x 10',
-	58: 'minutes', 59: 'Bitfield', 60: 's', 61: 'ms', 62: 'days',
-	63: 'RTC-Q', 64: 'Datetime'
+  0: '', 1: 'Wh', 2: 'kWh', 3: 'MWh', 4: 'GWh', 5: 'j', 6: 'kj', 7: 'Mj',
+  8: 'Gj', 9: 'Cal', 10: 'kCal', 11: 'Mcal', 12: 'Gcal', 13: 'varh',
+  14: 'kvarh', 15: 'Mvarh', 16: 'Gvarh', 17: 'VAh', 18: 'kVAh',
+  19: 'MVAh', 20: 'GVAh', 21: 'kW', 22: 'kW', 23: 'MW', 24: 'GW',
+  25: 'kvar', 26: 'kvar', 27: 'Mvar', 28: 'Gvar', 29: 'VA', 30: 'kVA',
+  31: 'MVA', 32: 'GVA', 33: 'V', 34: 'A', 35: 'kV',36: 'kA', 37: 'C',
+  38: 'K', 39: 'l', 40: 'm3', 41: 'l/h', 42: 'm3/h', 43: 'm3xC',
+  44: 'ton', 45: 'ton/h', 46: 'h', 47: 'hh:mm:ss', 48: 'yy:mm:dd',
+  49: 'yyyy:mm:dd', 50: 'mm:dd', 51: '', 52: 'bar', 53: 'RTC',
+  54: 'ASCII', 55: 'm3 x 10', 56: 'ton x 10', 57: 'GJ x 10',
+  58: 'minutes', 59: 'Bitfield', 60: 's', 61: 'ms', 62: 'days',
+  63: 'RTC-Q', 64: 'Datetime'
 }
 
 #######################################################################
@@ -120,167 +199,173 @@ def crc_1021(message):
 #
 
 escapes = {
-	0x06: True,
-	0x0d: True,
-	0x1b: True,
-	0x40: True,
-	0x80: True,
+  0x06: True,
+  0x0d: True,
+  0x1b: True,
+  0x40: True,
+  0x80: True,
 }
 
 #######################################################################
 # And here we go....
 #
-class kamstrup(object):
+class Kamstrup(object):
 
-	def __init__(self, serial_port = "/dev/cuaU0"):
-		self.debug_fd = open("/tmp/_kamstrup", "a")
-		self.debug_fd.write("\n\nStart\n")
-		self.debug_id = None
+  def __init__(self, serial_port = "/dev/cuaU0"):
+    #self.debug_fd = open("/tmp/_kamstrup", "a")
+    self.debug_fd = sys.stderr
+    self.debug_fd.write("\n\nStart\n")
+    self.debug_id = None
 
-		self.ser = serial.Serial(
-		    port = serial_port,
-		    baudrate = 1200,
-		    timeout = 1.0)
+    self.ser = serial.Serial(
+        port = serial_port,
+        baudrate = 1200,
+        timeout = 1.0)
 
-	def debug(self, dir, b):
-		for i in b:
-			if dir != self.debug_id:
-				if self.debug_id != None:
-					self.debug_fd.write("\n")
-				self.debug_fd.write(dir + "\t")
-				self.debug_id = dir
-			self.debug_fd.write(" %02x " % i)
-		self.debug_fd.flush()
+  def debug(self, dir, b):
+    for i in b:
+      if dir != self.debug_id:
+        if self.debug_id != None:
+          self.debug_fd.write("\n")
+        self.debug_fd.write(dir + "\t")
+        self.debug_id = dir
+      self.debug_fd.write(" %02x " % i)
+    self.debug_fd.flush()
 
-	def debug_msg(self, msg):
-		if self.debug_id != None:
-			self.debug_fd.write("\n")
-		self.debug_id = "Msg"
-		self.debug_fd.write("Msg\t" + msg)
-		self.debug_fd.flush()
+  def debug_msg(self, msg):
+    if self.debug_id != None:
+      self.debug_fd.write("\n")
+    self.debug_id = "Msg"
+    self.debug_fd.write("Msg\t" + msg)
+    self.debug_fd.flush()
 
-	def wr(self, b):
-		b = bytearray(b)
-		self.debug("Wr", b);
-		self.ser.write(b)
+  def wr(self, b):
+    b = bytearray(b)
+    self.debug("Wr", b);
+    self.ser.write(b)
 
-	def rd(self):
-		a = self.ser.read(1)
-		if len(a) == 0:
-			self.debug_msg("Rx Timeout")
-			return None
-		b = bytearray(a)[0]
-		self.debug("Rd", bytearray((b,)));
-		return b
+  def rd(self):
+    a = self.ser.read(1)
+    if len(a) == 0:
+      self.debug_msg("Rx Timeout")
+      return None
+    b = bytearray(a)[0]
+    self.debug("Rd", bytearray((b,)));
+    return b
 
-	def send(self, pfx, msg):
-		b = bytearray(msg)
+  def send(self, pfx, msg):
+    b = bytearray(msg)
 
-		b.append(0)
-		b.append(0)
-		c = crc_1021(b)
-		b[-2] = c >> 8
-		b[-1] = c & 0xff
+    b.append(0)
+    b.append(0)
+    c = crc_1021(b)
+    b[-2] = c >> 8
+    b[-1] = c & 0xff
 
-		c = bytearray()
-		c.append(pfx)
-		for i in b:
-			if i in escapes:
-				c.append(0x1b)
-				c.append(i ^ 0xff)
-			else:
-				c.append(i)
-		c.append(0x0d)
-		self.wr(c)
+    c = bytearray()
+    c.append(pfx)
+    for i in b:
+      if i in escapes:
+        c.append(0x1b)
+        c.append(i ^ 0xff)
+      else:
+        c.append(i)
+    c.append(0x0d)
+    self.wr(c)
 
-	def recv(self):
-		b = bytearray()
-		while True:
-			d = self.rd()
-			if d == None:
-				return None
-			if d == 0x40:
-				b = bytearray()
-			b.append(d)
-			if d == 0x0d:
-				break
-		c = bytearray()
-		i = 1;
-		while i < len(b) - 1:
-			if b[i] == 0x1b:
-				v = b[i + 1] ^ 0xff
-				if v not in escapes:
-					self.debug_msg(
-					    "Missing Escape %02x" % v)
-				c.append(v)
-				i += 2
-			else:
-				c.append(b[i])
-				i += 1
-		if crc_1021(c):
-			self.debug_msg("CRC error")
-		return c[:-2]
+  def recv(self):
+    b = bytearray()
+    while True:
+      d = self.rd()
+      ic(d)
+      if d == None:
+        return None
+      if d == 0x40:
+        ic()
+        b = bytearray()
+      b.append(d)
+      if d == 0x0d:
+        ic()
+        break
+    c = bytearray()
+    i = 1
+    while i < len(b) - 1:
+      if b[i] == 0x1b:
+        v = b[i + 1] ^ 0xff
+        if v not in escapes:
+          self.debug_msg(
+              "Missing Escape %02x" % v)
+        c.append(v)
+        i += 2
+      else:
+        c.append(b[i])
+        i += 1
+    if crc_1021(c):
+      self.debug_msg("CRC error")
+    ic(c)
+    return c#[:-2]
 
-	def readvar(self, nbr):
-		# I wouldn't be surprised if you can ask for more than
-		# one variable at the time, given that the length is
-		# encoded in the response.  Havn't tried.
+  def readvar(self, nbr):
+    # I wouldn't be surprised if you can ask for more than
+    # one variable at the time, given that the length is
+    # encoded in the response.  Havn't tried.
 
-		self.send(0x80, (0x3f, 0x10, 0x01, nbr >> 8, nbr & 0xff))
+    self.send(0x80, (0x3f, 0x10, 0x01, nbr >> 8, nbr & 0xff))
 
-		b = self.recv()
-		if b == None:
-			return (None, None)
+    b = self.recv()
+    if b == None:
+      return (None, None)
+    ic(b)
 
-		if b[0] != 0x3f or b[1] != 0x10:
-			return (None, None)
+    if b[0] != 0x3f or b[1] != 0x10:
+      return (None, None)
 
-		if b[2] != nbr >> 8 or b[3] != nbr & 0xff:
-			return (None, None)
+    if b[2] != nbr >> 8 or b[3] != nbr & 0xff:
+      return (None, None)
 
-		if b[4] in units:
-			u = units[b[4]]
-		else:
-			u = None
+    if b[4] in units:
+      u = units[b[4]]
+    else:
+      u = None
 
-		# Decode the mantissa
-		x = 0
-		for i in range(0,b[5]):
-			x <<= 8
-			x |= b[i + 7]
+    # Decode the mantissa
+    x = 0
+    for i in range(0,b[5]):
+      x <<= 8
+      x |= b[i + 7]
 
-		# Decode the exponent
-		i = b[6] & 0x3f
-		if b[6] & 0x40:
-			i = -i
-		i = math.pow(10,i)
-		if b[6] & 0x80:
-			i = -i
-		x *= i
+    # Decode the exponent
+    i = b[6] & 0x3f
+    if b[6] & 0x40:
+      i = -i
+    i = math.pow(10,i)
+    if b[6] & 0x80:
+      i = -i
+    x *= i
 
-		if False:
-			# Debug print
-			s = ""
-			for i in b[:4]:
-				s += " %02x" % i
-			s += " |"
-			for i in b[4:7]:
-				s += " %02x" % i
-			s += " |"
-			for i in b[7:]:
-				s += " %02x" % i
+    if False:
+      # Debug print
+      s = ""
+      for i in b[:4]:
+        s += " %02x" % i
+      s += " |"
+      for i in b[4:7]:
+        s += " %02x" % i
+      s += " |"
+      for i in b[7:]:
+        s += " %02x" % i
 
-			print(s, "=", x, units[b[4]])
+      print(s, "=", x, units[b[4]])
 
-		return (x, u)
-			
+    return (x, u)
+
 
 if __name__ == "__main__":
 
-	import time
+  import time
 
-	foo = kamstrup()
+  foo = Kamstrup(serial_port='/dev/ttyUSB2')
 
-	for i in kamstrup_382_var:
-		x,u = foo.readvar(i)
-		print("%-25s" % kamstrup_382_var[i], x, u)
+  for i in kamstrup_382_var:
+    x,u = foo.readvar(i)
+    print("%-25s" % kamstrup_382_var[i], x, u)
